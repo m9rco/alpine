a0e0e2ac60951e59d9dedb18217944b28e78ff9b
pushaowei <pushaowei0727@gmail.com>
2019-05-08 11:43:54 +0800

    fix: preparedness planning

--- 3.8/rootfs/etc/profile.d/welcome.sh
+++ 3.8/rootfs/etc/profile.d/welcome.sh
@@ -10,30 +10,3 @@ echo
 
 # colorized pretty print "username pwd $ "
 export PS1='\[\e]0;\u \w\a\]\[\033[01;32m\]\u \[\033[01;34m\]\w \$\[\033[00m\] '
-
-# See: https://unix.stackexchange.com/q/26676
-[[ $- == *i* ]] \
-    && echo 'This is an interactive shell' \
-    || echo 'This is not an interactive shell'
-
-# See: https://unix.stackexchange.com/q/26676
-shopt -q login_shell \
-    && echo 'This is a login shell' \
-    || echo 'This is not a login shell'
-
-echo 
-
-if [[ -f "/usr/share/entrypoint/unknown_args" ]]; then
-    echo "These unknown options were not processed"
-    cat /usr/share/entrypoint/unknown_args
-    echo
-fi
-
-
-
-if [[ -f "/usr/share/entrypoint/known_args" ]]; then
-    echo "Setting known flags"
-    cat /usr/share/entrypoint/known_args
-    . /usr/share/entrypoint/known_args
-    echo
-fi
